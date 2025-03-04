b41626e0ea2e0717e7648fc998a14b475352147c
Michael Sensenbrenner <z1947633@metis.niu.edu>
2025-03-04 17:47:03 -0600

    feat: Added PI & members sorting

--- get_projects.py
+++ get_projects.py
@@ -2,8 +2,9 @@
 import os
 import sys
 import subprocess
+import re
 
-def query_and_write_file(command, filename):
+def query_and_write_file(command, filename)-> str:
     '''
     query_and_write_file: Run a comand and write the results to a file
 
@@ -11,18 +12,57 @@ def query_and_write_file(command, filename):
     filename: File to be written to
     '''
     # Execute the command on machine
-    query_projects_output = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+    query_projects_output = subprocess.run(
+        command, shell=True, stdout=subprocess.PIPE, 
+        stderr=subprocess.PIPE, universal_newlines=True
+    )
     std_out = query_projects_output.stdout
 
-    # Debug code
-    # for line in std_out.splitlines():
-    #     print(line)
-
-    # Write the results to the file
     with open(filename, "a") as filename_description:
         filename_description.write(std_out)
 
     return std_out
+
+def sort_file_contents(filename):
+    '''
+    sort_file_contents: Read the contents of the file and sort its contents
+
+    filename: The file contents to be read
+    '''
+
+    # Stores all active PI's and their projects
+    active_metis_projects = {
+        "project_title": None,
+        "PI" : None, 
+        "group_member_count": None 
+    }
+
+    # Stores all Disabled PI's and their projects
+    archived_metis_projects = {
+        "project_title": None,
+        "PI" : None, 
+        "group_member_count": None 
+    }
+
+    extracted_data = {}
+
+    print("Sorting File Contents!")
+    with open(filename, "r") as file:
+
+        for line in file:
+
+            # Skip empty lines
+            if not line:
+                continue
+            
+            match = re.match(r"(\w+-(pi|members)):\s+(.*)", line)
+            if match:
+                key, _, value = match.groups()
+                extracted_data[key] = value.strip()
+
+        # Print extracted values
+        for key, value in extracted_data.items():
+            print(f"{key}: {value}")
 
 def main():
     '''
@@ -32,7 +72,6 @@ def main():
     print("Getting Metis Projects")
     
     # Query idap for project descriptions
-    # ldapsearch -Z -x -H ldap://pldapms.hpc.cls
     print("Getting Metis Project Descriptions")
     query_projects = "ldapsearch -Z -x -H ldap://pldapms.hpc.cls | grep description"
     query_and_write_file(query_projects, "metis_project_description.txt")
@@ -41,6 +80,14 @@ def main():
     print("Getting Metis Project Groups")
     metis_groups = "cat /home/admin/data/account_reports/aliases.ldap.Feb1_011701"
     query_and_write_file(metis_groups, "metis_project_groups.txt")
+
+    # Sort the contents of the file by the following
+    # Project title
+    # PI (Name and Department)
+    # Number of group members
+    # One paragraph of project description
+    # Link, if project is highlighted on the CRCD website
+    sort_file_contents("./metis_project_groups.txt")
 
 if __name__ == "__main__":
     '''
